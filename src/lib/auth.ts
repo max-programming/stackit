4eb94ca44db9b68098f4fd0ceab775f82c7ab6b1
Usman S. <ui.sabuwala@gmail.com>
2025-07-12 04:21:04 +0000

    Add tanstack start + better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import { reactStartCookies } from "better-auth/react-start";
 import * as authSchema from "./db/schema/auth";
 
 export const auth = betterAuth({
@@ -23,4 +24,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [reactStartCookies()],
 });
